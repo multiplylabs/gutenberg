cad761ffe7ec0a56cfc3e172992bf2fae48e2dcd
Miguel Fonseca <miguelcsf@gmail.com>
2018-02-14 18:04:39 +0000

    Parser: Remove support for <!--more-->

--- blocks/api/raw-handling/comment-remover.js
+++ blocks/api/raw-handling/comment-remover.js
@@ -8,5 +8,50 @@ export default function( node ) {
 		return;
 	}
 
+	if ( node.nodeValue.indexOf( 'more' ) === 0 ) {
+		// Grab any custom text in the comment
+		const customText = node.nodeValue.slice( 4 ).trim();
+
+		// When a <!--more--> comment is found, we need to look for any
+		// <!--noteaser--> sibling, but it may not be a direct sibling
+		// (whitespace typically lies in between)
+		let sibling = node;
+		let noTeaser = false;
+		while ( ( sibling = sibling.nextSibling ) ) {
+			if (
+				sibling.nodeType === COMMENT_NODE &&
+				sibling.nodeValue === 'noteaser'
+			) {
+				noTeaser = true;
+				sibling.parentNode.removeChild( sibling );
+				break;
+			}
+		}
+
+		// Conjure up a custom More element
+		const more = createMore( customText, noTeaser );
+
+		// Find the first ancestor to which the More element can be appended;
+		// appending to the closer P parents fails
+		let parent = node.parentNode;
+		while ( parent.nodeName.toLowerCase() === 'p' && parent.parentNode ) {
+			parent = parent.parentNode;
+		}
+		parent.appendChild( more );
+	}
+
 	node.parentNode.removeChild( node );
+}
+
+function createMore( customText, noTeaser ) {
+	const node = document.createElement( 'wp-block' );
+	node.dataset.block = 'core/more';
+	if ( customText ) {
+		node.dataset.customText = customText;
+	}
+	if ( noTeaser ) {
+		// "Boolean" data attribute
+		node.dataset.noTeaser = '';
+	}
+	return node;
 }
