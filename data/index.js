470bcb6e4d58d867d5776802c99780dbb9f61607
Andrew Duthie <andrew@andrewduthie.com>
2018-02-15 17:50:41 -0500

    Data: Specify store key in connected component
    Prevents conflicts with connected components competing for shared store

--- data/index.js
+++ data/index.js
@@ -117,14 +117,13 @@ export const query = ( mapSelectorsToProps ) => ( WrappedComponent ) => {
 			console.warn( 'Dispatch is not supported.' );
 		},
 	};
-	const connectWithStore = ( ...args ) => {
-		const ConnectedWrappedComponent = connect( ...args )( WrappedComponent );
-		return ( props ) => {
-			return <ConnectedWrappedComponent { ...props } store={ store } />;
-		};
-	};
 
-	return connectWithStore( ( state, ownProps ) => {
-		return mapSelectorsToProps( select, ownProps );
-	} );
+	const ConnectedWrappedComponent = connect(
+		( state, ownProps ) => mapSelectorsToProps( select, ownProps ),
+		null,
+		null,
+		{ storeKey: 'wpDataStore' }
+	)( WrappedComponent );
+
+	return ( props ) => <ConnectedWrappedComponent { ...props } wpDataStore={ store } />;
 };
